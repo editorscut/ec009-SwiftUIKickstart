202a8c4553c82cae5641c4948fd1a5ead3061a75
Daniel Steinberg <daniel@dimsumthinking.com>
2019-09-21 09:50:11 +0200

    Added 5-9 and 5-10 to draft

---
+++ 05/10/MyMood/MyMood/UI/MoodInput/MoodInputButton.swift
@@ -0,0 +1,42 @@
+//
+//  MoodInputButton.swift
+//  MyMood
+//
+//  Created by Daniel Steinberg on 9/19/19.
+//  Copyright © 2019 Dim Sum Thinking. All rights reserved.
+//
+
+import SwiftUI
+
+struct MoodInputButton: View {
+    @Binding var value: Double
+    @Binding var context: String
+    let history: History
+    
+    var inputIsNotValid: Bool {
+        context.isEmpty || (context.count > 30)
+    }
+    
+    var body: some View {
+        Button(action: recordMoodAndResetView){
+            Text("Record Mood")
+        }.disabled(inputIsNotValid)
+    }
+}
+
+extension MoodInputButton {
+    private func recordMoodAndResetView() {
+        history.add(rating: self.value,
+                    context: self.context)
+        value = 2.0
+        context = ""
+    }
+}
+
+struct MoodEntryButton_Previews: PreviewProvider {
+    static var previews: some View {
+        MoodInputButton(value: .constant(2.5),
+                        context: .constant("Just a stub"),
+                        history: History())
+    }
+}
